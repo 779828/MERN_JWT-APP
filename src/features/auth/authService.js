1506aa4583d32176fe380805b84d1e8d20c76905
Shashank <shashankbmusale07@gmail.com>
2025-01-16 16:15:16 +0530

    upd

--- src/features/auth/authService.js
+++ src/features/auth/authService.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import Cookies from "js-cookie";
 
-const API_URL = "/api/users/";
+const API_URL = "https://mern-jwt-api-nine.vercel.app/api/users/";
 
 // Register user
 const register = async (userData) => {
